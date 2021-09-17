4b80d72f460ed7661041b8519a15c5f180023294
SkillerEnte <LennySchurer@gmail.com>
2021-09-17 14:01:26 +0200

    edited SQL functionality (not done yet)

--- src/main/java/de/lunoro/locker/lock/LockSQLSerializer.java
+++ src/main/java/de/lunoro/locker/lock/LockSQLSerializer.java
@@ -1,6 +1,9 @@
 package de.lunoro.locker.lock;
 
 import de.lunoro.locker.sql.SQL;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,9 +18,14 @@ public class LockSQLSerializer {
 
     public void serialize(Lock lock) {
         // TODO: 16.09.2021 table
-        mySQL.update("INSERT INTO Locker (owner, xLocation, yLocation, zLocation ,trustedMembers) VALUES ('"
-                + lock.getOwner().toString() + "','" + lock.getBlockLocation().getBlockX() + "','" + lock.getBlockLocation().getBlockY() + "','" + lock.getBlockLocation().getBlockZ() + "','" + convertListToString(lock.getTrustedMembers()) + "')");
-        System.out.println();
+        mySQL.update("INSERT INTO Locker (owner, world, xLocation, yLocation, zLocation ,trustedMembers) VALUES ('"
+                + lock.getOwner().toString() +
+                "','" + lock.getBlockLocation().getExtent().getUniqueId().toString() +
+                "','" + lock.getBlockLocation().getBlockX() +
+                "','" + lock.getBlockLocation().getBlockY() +
+                "','" + lock.getBlockLocation().getBlockZ() +
+                "','" + convertListToString(lock.getTrustedMembers()) +
+                "')");
     }
 
     private String convertListToString(List<UUID> list) {
